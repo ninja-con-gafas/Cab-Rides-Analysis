650eca19fed1349da6a888c65d4f95a48c91d132
absdarekar <absdarekar01@gmail.com>
2022-09-09 12:53:59 +0530

    build: add terminal messages

--- run.sh
+++ run.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-# setup Apache Hadoop Distributed File System (HDFS) directories to import click stream data
+echo ">>> Setup Apache Hadoop Distributed File System (HDFS) directories to import click stream data"
 
 hdfs dfs -rm -r /user/livy
 hdfs dfs -mkdir -p /user/livy/click_stream_data
 
-# run the Apache Spark Structured Streaming application to read data from Apache Kafka topic and store as CSV files in
-# HDFS
+echo ">>> Run the Apache Spark Structured Streaming application to read data from Apache Kafka topic and store as CSV files in HDFS"
 
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5 data_sourcing/import_streaming_data.py
 
-# download and install MySQL connector in Amazon Elastic Map Reduce (EMR) master node
+echo ">>> Download and install MySQL connector in Amazon Elastic Map Reduce (EMR) master node"
 
 wget "https://de-mysql-connector.s3.amazonaws.com/mysql-connector-java-8.0.25.tar.gz"
 sudo rm /usr/lib/sqoop/lib/mysql-connector-java-8.0.25.jar
 sudo tar -xvf mysql-connector-java-8.0.25.tar.gz --strip-components=1 --directory /usr/lib/sqoop/lib/ mysql-connector-java-8.0.25/mysql-connector-java-8.0.25.jar
 
 
-# run an Apache Sqoop job to import bookings data from Amazon Relational Database Service (RDS) into HDFS
+echo ">>> Run the Apache Sqoop job to import bookings data from Amazon Relational Database Service (RDS) into HDFS"
 
 sqoop import \
 --connect jdbc:mysql://upgraddetest.cyaielc9bmnf.us-east-1.rds.amazonaws.com/testdatabase \
@@ -26,18 +25,17 @@ sqoop import \
 --target-dir /user/livy/bookings \
 -m 1
 
-# setup feeds path and copy data into the feeds path for ingestion of the imported data into Apache Hive
+echo ">>> Setup feeds path and copy data into the feeds path for ingestion of the imported data into Apache Hive"
 
 hdfs dfs -mkdir -p {/user/livy/feeds_path/click_stream_data,/user/livy/feeds_path/bookings}
 hdfs dfs -cp /user/livy/click_stream_data/*.csv /user/livy/feeds_path/click_stream_data
 hdfs dfs -cp /user/livy/bookings/part* /user/livy/feeds_path/bookings
 
-# ingest the click stream data and bookings data into Hive tables
+echo  ">>> Ingest the click stream data and bookings data into Hive tables"
 
 hive -f data_sourcing/ingest_streaming_data.sql
 hive -f data_sourcing/ingest_batch_data.sql
 
-# run Apache Spark application to aggregate bookings data, load the aggregated data into a Hive table and store as CSV
-# files in HDFS
+echo ">>> Run Apache Spark application to aggregate bookings data, load the aggregated data into a Hive table and store as CSV files in HDFS"
 
 spark-submit data_transformation/aggregate_batch_data.py
